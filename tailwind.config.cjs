bf48fa42764614f0a4b175bf327075942d671e60
Kaiano Levine <kaiano.levine@gmail.com.com>
2026-02-08 15:00:14 -0600

    updating

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  presets: [
+    require("@wcs-mn/eleventy-excellent-core/tailwind-preset")
+  ],
+
   content: [
     "./src/**/*.{njk,md,html,webc,liquid,js,ts}",
     "./node_modules/@wcs-mn/eleventy-excellent-core/src/**/*.{njk,md,html,webc,liquid,js,ts}"
@@ -9,11 +13,7 @@ module.exports = {
 
   theme: {
     extend: {
-      // Used by Eleventy Excellent styles (e.g. @screen ltsm)
-      screens: {
-        // "less than small" (< 640px)
-        ltsm: { max: "639px" }
-      }
+      // site-specific extensions go here
     }
   },
 
